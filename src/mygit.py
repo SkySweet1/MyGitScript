d0b91ee3393b85c589e9403c7e24e80b11780189
mashanov dmitry <mashanovhdhh@gmail.com>
2026-04-06 12:05:28 +0300

    added axperimental files and name of functions in main file

---
+++ src/mygit.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+def loadIgnorelist():
+
+#чтение .mygitignore
+
+def needIgnore():
+
+#нужно ли игнорировать конкретный файл (по пути)
+
+def getAllFiles():
+
+#возврат всех файлов в текущей дериктории (+игнорируемые)
+
+def getNextCommit():
+
+#возврат след номера комита
+
+def saveCommit():
+
+#сохранение всех файлов и метданных комита
+
+def restoreCommit():
+
+#замена директории на файлы из указанного комита
+
+def cmdInit():
+
+#инициализация репозитория
+
+def cmdCommit():
+
+#создание коммита
+
+def cmdCheckout():
+
+#переключение на коммит
+
+def main():
+
+#main
